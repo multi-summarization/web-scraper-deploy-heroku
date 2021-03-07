91f2bb0c2657acd290a10bee54976cbd806a4bb9
ryan-rozario <ryan.rozario1999@gmail.com>
2021-03-07 17:24:57 +0400

    news items structure

--- newsscraper/newsscraper/items.py
+++ newsscraper/newsscraper/items.py
@@ -5,10 +5,14 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class NewsscraperItem(scrapy.Item):
+class NewsscraperItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    source = Field()
+    headline = Field()
+    tagline = Field()
+    body = Field()
+    #genre = Field()
