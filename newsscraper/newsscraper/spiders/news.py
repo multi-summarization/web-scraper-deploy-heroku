5964c0727d773a3163aeb166d4873c180a783c52
Chinmayan Pradeep <chinmayanks@gmail.com>
2021-03-09 19:59:49 +0400

    hindustan times scraping under development. manual scraping might be necessary as the newspaper library is encountering a bug

--- newsscraper/newsscraper/spiders/news.py
+++ newsscraper/newsscraper/spiders/news.py
@@ -42,3 +42,42 @@ class ndtvSpider(scrapy.Spider):
         if next_page is not None:
             next_page_link = response.urljoin(next_page)
             yield scrapy.Request(url=next_page_link, callback=self.parse)
+
+# running: scrapy crawl hindu -o hindu_articles.json
+
+class hinduSpider(scrapy.Spider):
+    name = 'hindu'	
+
+    # def start_requests(self):
+        
+    start_urls = [
+        'https://www.hindustantimes.com/latest-news'
+    ]
+
+                
+    def parse(self, response):
+        for article in response.xpath("//div[@class='storyShortDetail']/descendant::a[not(parent::div)]"):
+            
+
+            ##### HEADLINE
+            headline=article.xpath("normalize-space(./text())").extract_first()
+
+            ##### LINK
+            link='https://www.hindustantimes.com' + article.xpath("./@href").extract_first()
+
+            ##### ARTICLE
+            article = fulltext(requests.get(link).text)
+
+
+            yield {
+                'headline' : headline,
+                'link' : link,
+                'article': article,
+
+            }
+    
+        # next_page = response.xpath("//li[@class='next']/a/@href").extract_first()
+        
+        # if next_page is not None:
+        #     next_page_link = response.urljoin(next_page)
+        #     yield scrapy.Request(url=next_page_link, callback=self.parse)
