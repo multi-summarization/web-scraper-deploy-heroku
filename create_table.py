55849251416ddc9f2c34542ecb6e434c589cdd76
ryan-rozario <ryan.rozario1999@gmail.com>
2021-05-11 22:01:12 +0400

    Errors added

--- create_table.py
+++ create_table.py
@@ -1,15 +1,16 @@
-from psycopg2 import connect
+from psycopg2 import connect, Error
 from os.path import join, dirname
 import os
 
-print(os.environ.get("DB_NAME"))
 
-
-connection = connect(database=os.environ.get("DB_NAME"), 
-                               user=os.environ.get("DB_USER"), 
-                               password=os.environ.get("DB_PASSWORD"), 
-                               host=os.environ.get("DB_HOST"))
-cursor = connection.cursor()
-cursor.execute("CREATE TABLE articles (id SERIAL PRIMARY KEY, source VARCHAR, headline VARCHAR, link VARCHAR,category VARCHAR, content VARCHAR)")
-connection.commit()
-connection.close()
+try:
+    connection = connect(os.environ.get("DATABASE_URL"), 
+                                user=os.environ.get("DB_USER"), 
+                                password=os.environ.get("DB_PASSWORD"), 
+                                host=os.environ.get("DB_HOST"))
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE articles (id SERIAL PRIMARY KEY, source VARCHAR, headline VARCHAR, link VARCHAR,category VARCHAR, content VARCHAR)")
+    connection.commit()
+    connection.close()
+except Error as e:
+    print(e)
--- delete_table.py
+++ delete_table.py
@@ -1,13 +1,15 @@
-from psycopg2 import connect
+from psycopg2 import connect, Error
 import os
 from os.path import join, dirname
 
-
-connection = connect(database=os.environ.get("DB_NAME"), 
-                               user=os.environ.get("DB_USER"), 
-                               password=os.environ.get("DB_PASSWORD"), 
-                               host=os.environ.get("DB_HOST"))
-cursor = connection.cursor()
-cursor.execute("TRUNCATE TABLE articles")
-connection.commit()
-connection.close()
+try:
+    connection = connect(database=os.environ.get("DB_NAME"), 
+                                user=os.environ.get("DB_USER"), 
+                                password=os.environ.get("DB_PASSWORD"), 
+                                host=os.environ.get("DB_HOST"))
+    cursor = connection.cursor()
+    cursor.execute("TRUNCATE TABLE articles")
+    connection.commit()
+    connection.close()
+except Error as e:
+    print(e)
